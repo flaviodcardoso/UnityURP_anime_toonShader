b9df683c21bc80cbc55433b02b01f10fcc49fb4a
flaviodcardoso <139507228+flaviodcardoso@users.noreply.github.com>
2026-06-09 16:49:42 +0100

    piece of code for URP asset
    This is the part we have to add to our urp-asset in order to making work the srp-batching with the outlines correctly.

---
+++ urp_asset_edit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+public class OutlineRenderFeature : ScriptableRendererFeature
+{
+    public override void Create() { }
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        // This ensures Outline pass is batched properly
+        var pass = new OutlinePass();
+        renderer.EnqueuePass(pass);
+    }
+    class OutlinePass : ScriptableRenderPass
+    {
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var sortingCriteria = renderingData.cameraData.defaultOpaqueSortFlags;
+            var drawingSettings = CreateDrawingSettings(new ShaderTagId("Outline"), ref renderingData, sortingCriteria);
+            drawingSettings.perObjectData = PerObjectData.None;
+            context.DrawRenderers(renderingData.cullResults, ref drawingSettings, ref filteringSettings);
+        }
+        FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.all);
+    }
+}
